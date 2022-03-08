4131cdea8cc2a2ae7e8644cb5eb660a6e5d6c9e0
momchil <momchil@momchils-MacBook-Air.local>
2022-03-08 17:37:16 +0100

    add storybook and components

---
+++ components/Headline/headline.stories.js
@@ -0,0 +1,6 @@
+import "./headline.css";
+export default {
+  title: "Components/button",
+};
+export const Default = () => `<button class="button">Click me</button>`;
+export const Big = () => `<button class="button button--big">Click me</button>`;
---
+++ components/buttons/buttons.stories.js
@@ -0,0 +1,7 @@
+import "./buttons.css";
+export default {
+  title: "Components/buttons.css",
+};
+export const Default = () => `<button class="button">Click me</button>`;
+export const Big = () => `<button class="button button--big">Click me</button>`;
+export const buttons = () => `<button class="buttonbig">Click me</button>`;
